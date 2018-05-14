65ea86edb79d04a5c995b5da568428ee6fa3c0f8
Mauricio Duran <mauriciodurant@gmail.com>
2018-05-13 22:17:35 -0400

    Se mejoran las migraciones con productos, roles y otros

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,17 +34,22 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                      <!-- pregunto si tiene permisos -->
+                      @can('products.index')
+                        <li class="nav-item">
+                          <a class="nav-link" href="{{ route ('products.index') }}">Productos</a>
+                        </li> 
+                      @endcan
+                      @can('users.index')
                       <li class="nav-item">
-                        <a class="nav-link" href={{ route ('products.index') }}>Productos</a>
+                        <a class="nav-link" href="{{ route ('users.index') }}">Usuarios</a>
                       </li> 
-
+                      @endcan
+                      @can('roles.index')
                       <li class="nav-item">
-                        <a class="nav-link" href={{ route ('users.index') }}>Usuarios</a>
+                        <a class="nav-link" href="{{ route ('roles.index') }}">Roles</a>
                       </li> 
-
-                      <li class="nav-item">
-                        <a class="nav-link" href={{ route ('roles.index') }}>Roles</a>
-                      </li> 
+                      @endcan
                     </ul>
 
                     <!-- Right Side Of Navbar -->
